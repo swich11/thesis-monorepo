c626cab518058c26063bb3ef25d7d7c70d75302b
Julian Britton <julian.m.britton@gmail.com>
2025-07-11 21:57:36 +1000

    stash, changing the event camera class to extend from the UW_Camera class, this gives depth and rgb images

--- OceanSim/isaacsim/oceansim/sensors/EventCamera.py
+++ OceanSim/isaacsim/oceansim/sensors/EventCamera.py
@@ -1,5 +1,16 @@
+# Omniverse Import
+import omni.replicator.core as rep
+import omni.ui as ui
+
+
 # Isaac sim import
 from isaacsim.sensors.camera import Camera
+
+
+import numpy as np
+import warp as wp
+import yaml
+import carb
 
 
 class EventCamera(Camera):
@@ -46,3 +57,119 @@ class EventCamera(Camera):
         super.__init__(prim_path, name, frequency, dt, resolution, position, translation, orientation, render_product_path)
 
 
+    def initialize(self,
+                   UW_param: np.ndarray = np.array([0.0, 0.31, 0.24, 0.05, 0.05, 0.2, 0.05, 0.05, 0.05 ]),
+                   viewport: bool = True,
+                   writing_dir: str = None,
+                   UW_yaml_path: str = None,
+                   physics_sim_view = None):
+
+        """Configure underwater rendering properties and initialize pipelines.
+
+        Args:
+            UW_param (np.ndarray, optional): Underwater parameters array:
+                [0:3] - Backscatter value (RGB)
+                [3:6] - Attenuation coefficients (RGB)
+                [6:9] - Backscatter coefficients (RGB)
+                Defaults to typical coastal water values.
+            viewport (bool, optional): Enable viewport visualization. Defaults to True.
+            writing_dir (str, optional): Directory to save camera events. Defaults to None.
+            UW_yaml_path (str, optional): Path to YAML file with water properties. Defaults to None.
+            physics_sim_view (_type_, optional): _description_. Defaults to None.            
+
+        """
+        self._id = 0
+        self._viewport = viewport
+        self._device = wp.get_preferred_device()
+        super().initialize(physics_sim_view)
+
+        if UW_yaml_path is not None:
+            with open(UW_yaml_path, 'r') as file:
+                try:
+                    # Load the YAML content
+                    yaml_content = yaml.safe_load(file)
+                    self._backscatter_value = wp.vec3f(*yaml_content['backscatter_value'])
+                    self._atten_coeff = wp.vec3f(*yaml_content['atten_coeff'])
+                    self._backscatter_coeff = wp.vec3f(*yaml_content['backscatter_coeff'])
+                    print(f"[{self._name}] On {str(self._device)}. Using loaded render parameters:")
+                    print(f"[{self._name}] Render parameters: {yaml_content}")
+                except yaml.YAMLError as exc:
+                    carb.log_error(f"[{self._name}] Error reading YAML file: {exc}")
+        else:
+            self._backscatter_value = wp.vec3f(*UW_param[0:3])
+            self._atten_coeff = wp.vec3f(*UW_param[6:9])
+            self._backscatter_coeff = wp.vec3f(*UW_param[3:6])
+            print(f'[{self._name}] On {str(self._device)}. Using default render parameters.')
+
+
+        # TODO: add event annotations
+
+        if self._viewport:
+            self.make_viewport()
+
+
+        if writing_dir is not None:
+            self._writing = True
+            self._writing_backend = rep.BackendDispatch({"paths": {"out_dir":writing_dir}})
+
+        print(f'[{self._name}] Initialized successfully. Data writing: {self._writing}')
+
+
+
+    def render(self):
+        """Process events for the time period of a single frame. Display the accumulated events in an image frame.
+
+        Note:
+            - Updates viewport display if enabled
+            - Saves image to disk if writing_dir was specified
+        """
+        
+
+
+
+
+
+
+    def make_viewport(self):
+        """
+            Create a viewport for real-time visualization of accumulated events as frames.
+        """
+        self.wrapped_ui_elements = []
+        self.window = ui.Window(self._name, width=self._resolution[0], height=self._resolution[1] + 40, visible=True)
+        self._provider = None # TODO: make ui.ImageProvider for event camera images
+        with self.window.frame:
+            with ui.ZStack(height=self._resolution[1]):
+                ui.Rectangle(style={"background_color": 0xFF000000})
+                ui.Label('Run the scenario for image to be received',
+                         style={'font_size': 55,'alignment': ui.Alignment.CENTER},
+                         word_wrap=True)
+                image_provider = ui.ImageWithProvider(self._provider, width=self._resolution[0], 
+                                                      height=self._resolution[1], 
+                                                      style={'fill_policy': ui.FillPolicy.PRESERVE_ASPECT_FIT,
+                                                      'alignment' :ui.Alignment.CENTER})
+        self.wrapped_ui_elements.append(image_provider)
+        self.wrapped_ui_elements.append(self._provider)
+        self.wrapped_ui_elements.append(self.window)
+
+
+    def close(self):
+        """Clean up resources by detaching annotators and clearing caches.
+    
+        Note:
+            - Required for proper shutdown when done using the sensor
+            - Also closes viewport window if one was created
+        """
+        # TODO: after adding annotators
+        if self._viewport:
+            self.ui_destroy()
+        
+        
+    def ui_destroy(self):
+        """Explicitly destroy viewport UI elements.
+    
+        Note:
+            - Called automatically by close()
+            - Only needed if manually managing UI lifecycle
+        """
+        for elem in self.wrapped_ui_elements:
+            elem.destroy()
