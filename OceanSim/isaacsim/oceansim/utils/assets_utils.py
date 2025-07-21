721a9ffe9f034fb66d269e4c78c9ed99005a5efd
Julian Britton <julian.m.britton@gmail.com>
2025-07-21 13:18:44 +1000

    grr

--- OceanSim/isaacsim/oceansim/utils/assets_utils.py
+++ OceanSim/isaacsim/oceansim/utils/assets_utils.py
@@ -3,3 +3,8 @@ from pathlib import Path
 
 def get_oceansim_assets_path() -> str:
     return str(Path(__file__).resolve().parent.parent.parent.parent / "assets")
+
+
+
+if __name__ == "__main__":
+    print(get_oceansim_assets_path())
